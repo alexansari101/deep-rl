36508d483ab68fe29a7676bce8df7b8d17b6b19f
Brad Saund <brad.saund@gmail.com>
2017-04-18 21:53:56 -0400

    Added utils for loading the correct directory

---
+++ util/dir_utils.py
@@ -0,0 +1,71 @@
+import os
+
+def get_last_experiment(parent_dir, env_name):
+    """Searches through directory for highest numbered experiement"""
+    experiment_id = 0
+    for folder_name in os.listdir(parent_dir):
+        if not os.path.isdir(os.path.join(parent_dir, folder_name)):
+            continue
+        try:
+            folder_name = int(folder_name.split('-run')[-1])
+            if folder_name > experiment_id:
+                experiment_id = folder_name
+        except:
+            pass
+    return experiment_id
+
+
+def get_output_folder(parent_dir, env_name):
+    """Return save folder.
+
+    Assumes folders in the parent_dir have suffix -run{run
+    number}. Finds the highest run number and sets the output folder
+    to that number + 1. This is just convenient so that if you run the
+    same script multiple times tensorboard can plot all of the results
+    on the same plots with different names.
+
+    Parameters
+    ----------
+    parent_dir: str
+      Path of the directory containing all experiment runs.
+
+    Returns
+    -------
+    parent_dir/run_dir
+      Path to this run's save directory.
+    """
+    os.makedirs(parent_dir, exist_ok=True)
+    experiment_id = get_last_experiment(parent_dir, env_name) + 1
+
+    parent_dir = os.path.join(parent_dir, env_name)
+    parent_dir = parent_dir + '-run{}'.format(experiment_id)
+    print('starting new trial ' + parent_dir)
+    return parent_dir
+
+def get_saved(parent_dir, env_name, exp_id=None, iter_num=None):
+    """Loads the model and hyperparams. By default, uses the last available data
+    """
+    if exp_id is None:
+        exp_id = get_last_experiment(parent_dir, env_name)
+        
+    parent_dir = os.path.join(parent_dir, env_name)
+    parent_dir = parent_dir + '-run{}'.format(exp_id)
+
+    # print(parent_dir)
+
+    
+    if iter_num is None:
+        for file_name in os.listdir(parent_dir):
+            # print('file...')
+            # print(file_name)
+            if not file_name.startswith('saved_model'):
+                continue
+            f_iter = int(file_name.split('saved_model_weights_')[-1])
+            if iter_num is None or \
+               f_iter > iter_num:
+                iter_num = f_iter
+        print('load last iteration: ' + str(iter_num))
+               
+            # print(f_iter)
+
+    return parent_dir, iter_num
