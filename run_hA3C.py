0b48c8e4c8547cea4c0643934aee68907ef0f817
Brad Saund <brad.saund@gmail.com>
2017-04-19 16:34:43 -0400

    implemented the test and train options

--- run_hA3C.py
+++ run_hA3C.py
@@ -83,32 +83,28 @@ def main():  # noqa: D103
         saver = tf.train.Saver(max_to_keep=5, keep_checkpoint_every_n_hours=1)
     with tf.Session() as sess:
 
-
-        # sess.run(tf.global_variables_initializer())
-        # init_adam = tf.initialize_variables([trainer])
-        # sess.run(init_adam)
         if args.load:
             # print('Loading Model ' + args.output)
             ckpt = tf.train.get_checkpoint_state(args.output)
             print('Loading Model ' + ckpt.model_checkpoint_path)
             saver.restore(sess,ckpt.model_checkpoint_path)
-            # for var in tf.global_variables():
-            #     try:
-            #         sess.run(var)
-            #     except tf.errors.FailedPreconditionError:
-            #         print(str(var) + ': UNUSABLE')
         else:
             sess.run(tf.global_variables_initializer())
+
+        if(args.train):
+            worker_threads = []
+            coord = tf.train.Coordinator()        
+            for worker in workers:
+                worker_work = lambda: worker.work(m_max_episode_length,update_ival,gamma,
+                                                  lam,master_network,sess,coord,saver)
+                t = threading.Thread(target=(worker_work))
+                t.start()
+                worker_threads.append(t)
+            coord.join(worker_threads)
             
-        worker_threads = []
-        coord = tf.train.Coordinator()        
-        for worker in workers:
-            worker_work = lambda: worker.work(m_max_episode_length,update_ival,gamma,
-                                              lam,master_network,sess,coord,saver)
-            t = threading.Thread(target=(worker_work))
-            t.start()
-            worker_threads.append(t)
-        coord.join(worker_threads)
+        if(args.test):
+            for i in range(100):
+                workers[0].evaluate(sess)
             
 if __name__ == '__main__':
     main()
